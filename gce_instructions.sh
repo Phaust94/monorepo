aaf7149f1f26cee4ffdfced0485cd848a10ac2d3
AntonB <mastakovsergei@gmail.com>
2023-10-22 22:50:34 +0200

    Added instructions to fix the instance if it dies

--- gce_instructions.sh
+++ gce_instructions.sh
@@ -1,2 +1,10 @@
 # Restart docker pull
 sudo systemctl start konlet-startup
+
+# If instance dies
+# Mount disk
+sudo mount -o discard,defaults /dev/sdb /mnt/disks/disk-bots
+# Fix permissions
+sudo chown -R 1000:1000 /mnt/disks/disk-bots/
+# Restart docker container
+sudo systemctl start konlet-startup
